11df2cf91d2dbf2cbe7a4b3817ba3711e207b79c
Никита Сычев <nsychev2023@LI2.local>
2022-03-11 15:55:57 +0300

    new changes

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('404', views.getnotFound),
+    path('<name>', views.janrGetName),
+    path('', views.mainstr, name='main'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.template.loader import render_to_string
 
+janrs = {'punk': 'punk is type'}
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def mainstr(request):
+    return HttpResponse(render_to_string('polls/structure.html'))
+
+def janrGetName(request, name):
+    if janrs.get(name):
+        return HttpResponse(janrs[name])
+    else:
+        return HttpResponseRedirect('404')
+
+def getnotFound(request):
+    return HttpResponse(render_to_string('polls/404.html'))

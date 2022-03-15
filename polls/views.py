782b8f90183ad0a9538507a188463998f3c026cf
Никита Сычев <nsychev2023@LI2.local>
2022-03-15 14:52:23 +0300

    third

--- polls/views.py
+++ polls/views.py
@@ -3,17 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.template.loader import render_to_string
+from django.shortcuts import render
 
-janrs = {'punk': 'punk is type'}
+janrs = {'punk': 'punk is type',
+         'alternative': "жанр"
+}
 
 def mainstr(request):
-    return HttpResponse(render_to_string('polls/structure.html'))
+    return render(request, 'polls/structure.html',)
 
 def janrGetName(request, name):
     if janrs.get(name):
-        return HttpResponse(janrs[name])
+        data = {
+        "janr": name,
+        "description": janrs[name]
+    }
+        return render(request, 'polls/second.html', context=data)
     else:
         return HttpResponseRedirect('404')
 
 def getnotFound(request):
-    return HttpResponse(render_to_string('polls/404.html'))
+    return render(request, 'polls/404.html')

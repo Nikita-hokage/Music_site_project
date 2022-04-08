ddeae5c3340abbc0ddcccc26bc401a510720c039
Никита Сычев <nsychev2023@LI2.local>
2022-04-08 16:35:23 +0300

    added dadtabase

--- musicsite/models.py
+++ musicsite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Janrs(models.Model):
+    janr = models.CharField
+    description = models.CharField
+    authors = models.CharField
